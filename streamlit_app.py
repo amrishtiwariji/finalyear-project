7c274d17626666a45e649d82c8ddfbd6257a2870
Amrish <140809606+amrishtiwariji@users.noreply.github.com>
2025-09-14 13:40:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from file_checker import checkFile
 
 st.title("Malware Detection using Machine Learning")
 st.markdown(""" 
-Malwares can wreak havoc on a computer and its network. Hackers use it to steal passwords, delete files and render computers inoperable. A malware infection can cause many problems that affect daily operation and the long-term security of your company.
+Malwares can steal your personal data from a computer and its network. Hackers use it to steal passwords, delete files from devices or steal information for financial gain or sabotage. A malware infection can cause many problems that affect daily operation and the long-term security of your company.
 
 This is a python program for detecting whether a given file is 
 a probable malware or not! \.""")
